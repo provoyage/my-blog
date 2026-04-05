248ad55f615e0fd766c89cedfb0c6ca185ec11f9
PROVOYAGE <info@qutech.jp>
2026-04-05 21:14:09 +0900

    Separate Sanity Studio

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,12 +1,8 @@
-import { defineCliConfig } from "sanity/cli";
-
-const sanityProjectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? process.env.SANITY_PROJECT_ID ?? "";
-const sanityDataset =
-  process.env.NEXT_PUBLIC_SANITY_DATASET ?? process.env.SANITY_DATASET ?? "production";
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: sanityProjectId || "placeholder",
-    dataset: sanityDataset,
-  },
-});
+    projectId: 'l1iirws1',
+    dataset: 'production'
+  }
+})
